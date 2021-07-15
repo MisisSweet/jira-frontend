362e81d77d526222840a76a6e13da2451ce63955
MisisSweet <margarita_semashko@mail.ru>
2021-07-15 17:49:22 +0300

    add work hours

--- src/components/calendar/day-component.tsx
+++ src/components/calendar/day-component.tsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { TypeDay } from "../../services/data/typeDay";
 import { Day } from "../../services/models/Calendar/day";
 
 interface DayComponentProps {
@@ -15,14 +16,41 @@ export class DayComponent extends Component<DayComponentProps> {
             && currentDate.getDate() === date.getDate();
     }
 
+    getClassOfTypeDay() {
+        const { day, date } = this.props;
+
+        if (date.getDay() === 6 || date.getDay() === 0) {
+            return 'calendar-day-holiday';
+        }
+
+        if (day) {
+            switch (day.type) {
+                case TypeDay.HOLIDAY:
+                    return 'calendar-day-holiday';
+                case TypeDay.SHORT_DAY:
+                    return 'calendar-day-short';
+                case TypeDay.SICK_DAY:
+                    return 'calendar-day-sick';
+                case TypeDay.VACATION:
+                    return 'calendar-day-vacation';
+            }
+        }
+        return '';
+    }
+
     render() {
-        const { date, disable } = this.props;
+        const { date, disable, day } = this.props;
         const disableClass = disable ? 'calendar-day-disable' : '';
         const holidayClass = date.getDay() === 6 || date.getDay() === 0 ? 'calendar-day-holiday' : '';
         const currentClass = this.checkCurrentDay(date) ? 'calendar-day-current' : '';
         return (
             <td className={`calendar-day ${disableClass} ${holidayClass} ${currentClass}`}>
-                {date ? date.getDate() : 'x'}
+                <p className="calendar-day-date">
+                    {date ? date.getDate() : 'x'}
+                </p>
+                <small className="calendar-day-work-hour">
+                    {day ? day.workHours : 0}
+                </small>
             </td>
         )
     }
